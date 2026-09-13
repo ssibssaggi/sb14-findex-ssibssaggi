08f3296763b9b73e579075f2f1ed419dbc57a91e
EUNSOLY <dmsthf9596@gmail.com>
2026-09-13 16:04:49 +0900

    ✨feat: 성과 랭킹조회 QueryDSL문 생성
    1. 휴일이라면 이전 데이터를 가져와야하하기 때문에 loe 조건절로 제일 가까운 날짜 도출 쿼리
    2. 기본 날짜를 기반으로 현재 데이터 도출 쿼리
    3. 기본 날짜를 기반으로 이전 기간 데이터 도출 쿼리

--- src/main/java/com/ssibssaggi/findex/repository/IndexDataCustomRepository.java
+++ src/main/java/com/ssibssaggi/findex/repository/IndexDataCustomRepository.java
@@ -1,5 +1,26 @@
 package com.ssibssaggi.findex.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import com.ssibssaggi.findex.domain.entity.index.IndexData;
+import com.ssibssaggi.findex.domain.entity.index.PeriodType;
+
 public interface IndexDataCustomRepository {
 
+    Optional<LocalDate> findTargetDate(LocalDate today, Long indexInfoId);
+
+    public List<IndexData> findByDate(
+            LocalDate targetDate,
+            Long indexInfoId,
+            Integer limit
+    );
+
+    List<IndexData> findByDateAndPeriod(
+            LocalDate today,
+            Long indexInfoId,
+            PeriodType periodType,
+            Integer limit
+    );
 }
